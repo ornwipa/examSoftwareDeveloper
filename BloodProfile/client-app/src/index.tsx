108d1233fe1536535a0682ef7a4325a8403a2f28
ornwipa <fahornwipa@gmail.com>
2021-01-12 20:11:43 -0600

    clean codes

--- BloodProfile/client-app/src/index.tsx
+++ BloodProfile/client-app/src/index.tsx
@@ -1,26 +1,10 @@
 import './index.css';
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
-*/
-
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
-/*
-import { BloodWork, BloodWorkList } from './components/BloodWork';
-*/
 
 export const routes = <Layout>
-    <Route path='api/bloodwork' />
+    <Route path='bloodwork' />
 </Layout>;
+
+reportWebVitals();
